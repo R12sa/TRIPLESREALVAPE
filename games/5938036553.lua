f4a4ae747e535e7b3b5b0aa507c7dd54ce054477
R12sa <144062428+R12sa@users.noreply.github.com>
2025-03-31 21:31:47 -0700

    Update 5938036553.lua

--- games/5938036553.lua
+++ games/5938036553.lua
@@ -12,25 +12,27 @@ local isfile = isfile or function(file)
 	return suc and res ~= nil and res ~= ''
 end
 local function downloadFile(path, func)
-	if not isfile(path) then
-		local suc, res = pcall(function() 
-			return game:HttpGet('https://raw.githubusercontent.com/pifaifiohawiohh8924920904444ffsfszcz/DHOHDOAHDA-HDDDA/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true) 
-		end)
-		if not suc or res == '404: Not Found' then 
-			error(res) 
-		end
-		if path:find('.lua') then 
-			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res 
-		end
-		writefile(path, res)
-	end
-	return (func or readfile)(path)
+    if not isfile(path) then
+        local suc, res = pcall(function() 
+            return game:HttpGet('https://raw.githubusercontent.com/R12sa/TRIPLESREALVAPE/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+        end)
+        if not suc or res == '404: Not Found' then 
+            error(res)
+        end
+        if path:find('.lua') then
+            res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+        end
+        writefile(path, res)
+    end
+    return (func or readfile)(path)
 end
-local run = function(func) 
-	func() 
+
+local run = function(func)
+    func()
 end
-local cloneref = cloneref or function(obj) 
-	return obj 
+
+local cloneref = cloneref or function(obj)
+    return obj
 end
 
 local playersService = cloneref(game:GetService('Players'))
