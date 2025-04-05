e970f662a2c8cd5142881638e7946439fb5b9cb3
R12sa <144062428+R12sa@users.noreply.github.com>
2025-04-05 16:31:36 -0700

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -50,14 +50,8 @@ for _, folder in pairs({'newvape', 'newvape/games', 'newvape/profiles', 'newvape
     end
 end
 
--- Set default GUI to new if not set
-if not isfile('newvape/profiles/gui.txt') then
-    pcall(function() writefile('newvape/profiles/gui.txt', 'new') end)
-end
-
 -- Function to load the main script
 local function loadMainScript()
-    -- First, check for updates and set commit
     if not shared.VapeDeveloper then
         local retries = 3
         local subbed
@@ -89,190 +83,14 @@ local function loadMainScript()
             end
             
             pcall(function() writefile('newvape/profiles/commit.txt', commit) end)
-        else
-            -- If we can't get the commit, just use main
-            pcall(function() writefile('newvape/profiles/commit.txt', 'main') end)
         end
     end
-
-    -- Download and write a fixed main.lua that handles the utils_functions issue
-    local fixedMainLua = [[--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.
-repeat task.wait() until game:IsLoaded()
-if shared.vape then shared.vape:Uninject() end
-
--- Loading the crash prevention script
-pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/R12sa/TRIPLESREALVAPE/main/CrashPrevention.lua"))() end)
-
--- why do exploits fail to implement anything correctly? Is it really that hard?
-if identifyexecutor then
-    if table.find({'Argon', 'Wave'}, ({identifyexecutor()})[1]) then
-        getgenv().setthreadidentity = nil
-    end
-end
-
-local vape
-local loadstring = function(...)
-    local res, err = loadstring(...)
-    if err and vape then
-        vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert')
-    end
-    return res
-end
-
-local queue_on_teleport = queue_on_teleport or function() end
-
-local isfile = isfile or function(file)
-    local suc, res = pcall(function()
-        return readfile(file)
-    end)
-    return suc and res ~= nil and res ~= ''
-end
-
-local cloneref = cloneref or function(obj)
-    return obj
-end
-
-local playersService = cloneref(game:GetService('Players'))
-
-local function downloadFile(path, func)
-    if not isfile(path) then
-        local suc, res = pcall(function()
-            return game:HttpGet('https://raw.githubusercontent.com/R12sa/TRIPLESREALVAPE/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
-        end)
-        if not suc or res == '404: Not Found' then
-            error(res)
-        end
-        if path:find('.lua') then
-            res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
-        end
-        writefile(path, res)
-    end
-    return (func or readfile)(path)
-end
-
-local function finishLoading()
-    vape.Init = nil
-    vape:Load()
-    task.spawn(function()
-        repeat
-            vape:Save()
-            task.wait(10)
-        until not vape.Loaded
-    end)
-    local teleportedServers
-    vape:Clean(playersService.LocalPlayer.OnTeleport:Connect(function()
-        if (not teleportedServers) and (not shared.VapeIndependent) then
-            teleportedServers = true
-            local teleportScript = "script_key = '"..(getgenv().script_key or "")..[[';
-            getgenv().script_key = script_key
-            
-            shared.vapereload = true
-            
-            if shared.VapeDeveloper then
-                loadstring(readfile('newvape/loader.lua'), 'loader')()
-            else
-                loadstring(game:HttpGet('https://raw.githubusercontent.com/R12sa/TRIPLESREALVAPE/'..readfile('newvape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
-            end
-            ]]
-            if shared.VapeDeveloper then
-                teleportScript = 'shared.VapeDeveloper = true\n'..teleportScript
-            end
-            if shared.VapeCustomProfile then
-                teleportScript = 'shared.VapeCustomProfile = "'..shared.VapeCustomProfile..'"\n'..teleportScript
-            end
-            vape:Save()
-            queue_on_teleport(teleportScript)
-        end
-    end))
-    if not shared.vapereload then
-        if not vape.Categories then return end
-        if vape.Categories.Main.Options['GUI bind indicator'].Enabled then
-            vape:CreateNotification('Finished Loading', vape.VapeButton and 'Press the button in the top right to open GUI' or 'Press '..table.concat(vape.Keybind, ' + '):upper()..' to open GUI', 5)
-        end
-    end
-end
-
-if not isfile('newvape/profiles/gui.txt') then
-    writefile('newvape/profiles/gui.txt', 'new')
-end
-
-local gui = readfile('newvape/profiles/gui.txt')
-if not isfolder('newvape/assets/'..gui) then
-    makefolder('newvape/assets/'..gui)
-end
-
-vape = loadstring(downloadFile('newvape/guis/'..gui..'.lua'), 'gui')()
-shared.vape = vape
-
-local XFunctions = loadstring(downloadFile('newvape/libraries/XFunctions.lua'), 'XFunctions')()
-XFunctions:SetGlobalData('XFunctions', XFunctions)
-XFunctions:SetGlobalData('vape', vape)
-
-local PerformanceModule = loadstring(downloadFile('newvape/libraries/performance.lua'), 'Performance')()
-XFunctions:SetGlobalData('Performance', PerformanceModule)
-
--- Fixed utils_functions loading with error handling
-local utils_functions
-local success, result = pcall(function()
-    return loadstring(downloadFile('newvape/libraries/utils.lua'), 'Utils')()
-end)
-
-if success and result then
-    utils_functions = result
-    for i, v in pairs(utils_functions or {}) do
-        getfenv()[i] = v
-    end
-else
-    warn("Failed to load utils_functions: " .. tostring(result))
-end
-
-getgenv().InfoNotification = function(title, msg, dur)
-    warn('info', tostring(title), tostring(msg), tostring(dur))
-    vape:CreateNotification(title, msg, dur)
-end
-
-getgenv().warningNotification = function(title, msg, dur)
-    warn('warn', tostring(title), tostring(msg), tostring(dur))
-    vape:CreateNotification(title, msg, dur, 'warning')
-end
-
-getgenv().errorNotification = function(title, msg, dur)
-    warn("error", tostring(title), tostring(msg), tostring(dur))
-    vape:CreateNotification(title, msg, dur, 'alert')
-end
-
-if not shared.VapeIndependent then
-    loadstring(downloadFile('newvape/games/universal.lua'), 'universal')()
-    loadstring(downloadFile('newvape/games/modules.lua'), 'modules')()
-    if isfile('newvape/games/'..game.PlaceId..'.lua') then
-        loadstring(readfile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
-    else
-        if not shared.VapeDeveloper then
-            local suc, res = pcall(function()
-                return game:HttpGet('https://raw.githubusercontent.com/R12sa/TRIPLESREALVAPE/'..readfile('newvape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
-            end)
-            if suc and res ~= '404: Not Found' then
-                loadstring(downloadFile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
-            end
-        end
-    end
-    finishLoading()
-else
-    vape.Init = finishLoading
-    return vape
-end
-
-shared.VapeFullyLoaded = true
-]]
-
-    -- Write the fixed main.lua
-    pcall(function() writefile('newvape/fixed_main.lua', fixedMainLua) end)
 
     -- Load script safely
     local success, err = pcall(function()
-        loadstring(readfile('newvape/fixed_main.lua'), 'fixed_main')()
+        loadstring(downloadFile('newvape/main.lua'), 'main')()
     end)
-    
+
     if not success then
         warn("Failed to load script: " .. tostring(err))
         game.StarterGui:SetCore("SendNotification", {
@@ -284,7 +102,7 @@ shared.VapeFullyLoaded = true
     else
         game.StarterGui:SetCore("SendNotification", {
             Title = "Success",
-            Text = "Script loaded successfully!",
+            Text = "Shop script loaded successfully!",
             Duration = 2
         })
         return true
@@ -362,3 +180,4 @@ game:GetService("RunService").Heartbeat:Connect(function()
         end
     end
 end)
+
